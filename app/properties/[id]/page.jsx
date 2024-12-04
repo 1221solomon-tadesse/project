bd12c561e85c885e649549eb4856ba58a2a48c09
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-12-04 21:32:31 +0300

    updated

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -9,7 +9,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 //  import PropertyImages from '@/components/PropertyImages';
 //  import BookmarkButton from '@/components/BookmarkButton';
 //  import PropertyContactForm from '@/components/PropertyContactForm';
-//  import ShareButtons from '@/components/ShareButtons';
+ import ShareButtons from '@/components/ShareButtons';
  import Spinner from '@/components/Spinner';
  import { FaArrowLeft } from 'react-icons/fa';
 
@@ -69,8 +69,8 @@ const PropertyPage = () => {
                   <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
                      <PropertyDetails property={property} />
                      <aside className='space-y-4'>
-                       {/* <BookmarkButton property={property} /> */}
-                       {/* <ShareButtons property={property} /> */}
+                       {/* <BookmarkButton property={property} />  */}
+                       <ShareButtons property={property} />
                        {/* <PropertyContactForm property={property} /> */}
                      </aside>
                    </div>
