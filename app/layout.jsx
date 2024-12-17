0a6d04e68bb70ebe37f7f482f714156ef435d0e6
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-12-17 16:05:54 +0300

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,23 +2,23 @@
 import "@/assets/styles/globals.css"
 import Footer from "@/components/Footer";
 import Navbar from '@/components/navbar';
+import AuthProvider from "@/components/AuthProvider";
 export const metadata={
   title: 'soll_dev startapp',
   description:' the project is for rental properties',
 }
 const layout = ({children}) => {
   return (
-    <html>    
-      <body>
-      <Navbar/>
-       <main>
-      {children}
-      </main>
-      <Footer/>
-        </body> 
-   
-    </html>
-  )
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
+  );
 }
 
 export default layout
