dfc24b1fc3e32cfc646019e62e2a2b52d6d210eb
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-12-18 11:09:04 +0300

    updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import { authOptions } from "@/utils/outhOptions";
+import { authOptions } from '@/utils/outhOption'
 import NextAuth from "next-auth/next";
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
