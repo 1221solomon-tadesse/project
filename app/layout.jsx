b69a1acc727a816d5b1faf838735d738c05e89a8
1221solomon-tadesse <solomontadesse480@gmail.com>
2025-03-30 22:19:03 +0300

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,33 @@
 
-import '@/assets/styles/globals.css';
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Navbar from '@/components/navbar';
 import AuthProvider from "@/components/AuthProvider";
-export const metadata={
-  title: 'soll_dev startapp',
-  description:' the project is for rental properties',
-}
-const layout = ({children}) => {
-  return (
-    <AuthProvider>
-      <html>
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-        </body>
-      </html>
-    </AuthProvider>
-  );
-}
+import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
+import "@/assets/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 
-export default layout
+export const metadata = {
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
+  );
+};
+export default MainLayout;
